601b764750befdec4b9e45e7b0ef96c07ab76a40
dhruva7vernekar-blip <dhruva7vernekar@gmail.com>
2025-10-04 11:20:59 +0530

    saving

--- 5.py
+++ 5.py
@@ -2,3 +2,6 @@ balance = int (input("Enter initial balance: "))
 deposit = int (input("Enter deposit amount: "))
 balance += deposit
 print("Balance after deposit:", balance)
+withdraw = int (input("Enter withdraw amount: "))
+balance -= deposit
+print("Balance after withdraw:",balance)
